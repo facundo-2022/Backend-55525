bc1765999e693c0e993f7629cfa8d8b7b379d4c6
facundometzler <fmetzler22@gmail.com>
2023-08-14 21:18:25 -0300

    proyecto

--- Clase 11/server.js
+++ Clase 11/server.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const http = require('http');
-const sockerIo =  require('sockert.io');
-const path = require('path');
-const { server} = require('socket.io');
-const handlebars = require('express-handlebars');
-const swal = require('sweetalert2');
-
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+const { Server } = require("socket.io");
+const handlebars = require("express-handlebars");
+const Swal = require("sweetalert2");
+const { CLIENT_RENEG_LIMIT } = require("tls");
 
 const app = express();
-const server = http.createServer(app)
-const io = new Server (server);
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.engine("handlebars", handlebars.engine())
-app.set("views",__dirname + "/views")
-app.set("view engine", "handlebars")
-app.use(express.static(path.join(__dirname,'public')))
+//configuracion de traer los documentos de nuestro directorio de views y publics
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+//el mieldword para express
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render("index.hbs")
-})
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
 
-const users = {}
+const users = {};
 
-io.on("connection",(socket)=>{
-    console.log("un usuario se ha conectado")
-    socket.on("new-user", (usernames)=>{}
-    users[]
-})
+io.on("connection", (socket) => {
+  console.log("un usuario se ha conectado");
+  socket.on("new-user", (username) => {
+    users[socket.id] = username;
+    io.emit("userConnected", username);
+  });
+
+  socket.on("chatMessage", (message) => {
+    const username = users[socket.id];
+    io.emit("message", { username, message });
+  });
+
+  socket.on("disconnect", () => {
+    const username = users[socket.id];
+    delete users[socket.id];
+    io.emit("userDisconnected", username);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
